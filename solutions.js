01fec2e068d2a9e0bdf8cb2b630a273f74f37226
SpanishJoplin <jpreynoldsku@gmail.com>
2018-03-22 16:52:51 -0600

    Commit 1 - Verbal Questions

---
+++ solutions.js
@@ -0,0 +1,9 @@
+//Verbal Questions
+
+//A parameter is the variable you want to change in a function
+//ex. const myFunction = (parameter) => {}
+//An argument is the word you put inside the parameters when you call the function
+//ex. myFunction(argument);
+
+//When you type console.log(value). It will show you the value in the console.
+//When you type return it will actually spit out the value in the program instead of resulting undefined. 
